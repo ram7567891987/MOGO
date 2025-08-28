221830b14f8761a4cb0e69a001ebc966162c6600
ram7567891987 <ramsattori999@gmail.com>
2025-08-28 11:09:19 +0500

    Implement fixed header functionality and smooth scrolling: add JavaScript for header class toggle and navigation link activation

---
+++ js/app.js
@@ -0,0 +1,55 @@
+$(function() {
+
+
+    var header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrollOffset = $(window).scrollTop();
+
+
+
+        // Fixed Header
+        checkScroll(scrollOffset);
+
+    $(window).on("scroll", function() {
+
+        scrollOffset = $(this).scrollTop();
+
+        checkScroll(scrollOffset);
+
+    });
+
+    function checkScroll(scrollOffset) {
+
+        if (scrollOffset >= introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed")
+        }
+    }
+
+    // Smooth Scroll
+
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+
+        let $this = $(this),
+            blockId = $(this).data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+            $("#nav a").removeClass("active")
+            $this.addClass("active");
+
+            $("html, body").animate({
+                scrollTop: blockOffset
+
+            }, 500);
+
+    });
+
+
+
+
+
+
+});
